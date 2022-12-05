2172595fda96e4d79b089e43e3af874cbc66c674
Your Name <you@example.com>
2022-12-05 22:00:32 +0300

    smartedu project

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,7 +1,20 @@
-exports.getIndexPage = (req, res) => {
-  console.log(req.session.userID)
+const nodemailer = require("nodemailer");
+const Course = require('../models/Course');
+const User = require('../models/User');
+
+exports.getIndexPage = async(req, res) => {
+
+  const courses = await Course.find().sort('-createdAt').limit(2);
+  const totalCourses = await Course.find().countDocuments();
+  const totalStudents = await User.countDocuments({role: 'student'});
+  const totalTeachers = await User.countDocuments({role: 'teacher'});
+
   res.status(200).render('index', {
     page_name: 'index',
+    courses,
+    totalCourses,
+    totalStudents,
+    totalTeachers
   });
 };
 
@@ -23,3 +36,61 @@ exports.getLoginPage = (req, res) => {
   });
 };
 
+exports.getContactPage = (req, res) => {
+  res.status(200).render('contact', {
+    page_name: 'contact',
+  });
+};
+
+exports.sendEmail = async (req, res) => {
+
+  try {
+  
+  const outputMessage = `
+
+
+  <h1>Mail Details </h1>
+  <ul>
+    <li>Name: ${req.body.name} </li>
+    <li>Email: ${req.body.email}</li>
+  </ul>
+  <h1>Message</h1>
+  <p>${req.body.message}</p>
+
+
+  `
+
+  let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: "burakksanver@gmail.com", // gmail account
+      pass: "sqirjvomdeainsrw111", // gmail password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Smart EDU Contact Form" <burakksanver@gmail.com>', // sender address
+    to: "davuulcuu@gmail.com", // list of receivers
+    subject: "Smart EDU Contact Form New Message ✔", // Subject line
+    html: outputMessage, // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+  req.flash("success", "We received your message succesfully");
+
+  res.status(200).redirect('contact')
+
+} catch (err) {
+  //req.flash("error", `Something happened! ${err}`);
+  req.flash("error", `Something happened!`);
+  res.status(200).redirect('contact')
+}
+};
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const courseController = require('../controllers/courseController');
+const roleMiddleware = require('../middlewares/roleMiddleware')
 
 const router = express.Router();
 
-router.route('/').post(courseController.createCourse);  // http://localhost:3000/courses
-//router.route('/yeni').post(courseController.yeniKurs);  // http://localhost:3000/courses/yeni
+router.route('/').post(roleMiddleware(["teacher", "admin"]), courseController.createCourse);  // http://localhost:3000/courses
 router.route('/').get(courseController.getAllCourses);  // http://localhost:3000/courses
 router.route('/:slug').get(courseController.getCourse);  // http://localhost:3000/courses
-
+router.route('/:slug').delete(courseController.deleteCourse); 
+router.route('/:slug').put(courseController.updateCourse); 
+router.route('/enroll').post(courseController.enrollCourse);  // http://localhost:3000/courses/enroll
+router.route('/release').post(courseController.releaseCourse);  // http://localhost:3000/courses/enroll
 module.exports = router;
